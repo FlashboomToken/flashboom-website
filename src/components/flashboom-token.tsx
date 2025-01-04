82a2a08e388d32a319a296681b09922bca8c9f98
FlashboomToken <cafdu69@gmail.com>
2025-01-04 20:03:37 +0000

    Add components folder with flashboom-token component

---
+++ src/components/flashboom-token.tsx
@@ -0,0 +1,12 @@
+// src/components/flashboom-token.tsx
+
+import React from 'react';
+
+export default function FlashBoomToken() {
+  return (
+    <section>
+      <h2>FlashBoom Token</h2>
+      <p>This is our main component!</p>
+    </section>
+  );
+}
